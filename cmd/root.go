7bbcab290b3c04294a00aedbaf7d6d7a510f00d7
Kristoffer Dalby <kradalby@kradalby.no>
2018-09-29 18:33:59 +0200

    adds description

--- cmd/root.go
+++ cmd/root.go
@@ -37,15 +37,8 @@ var TemplateFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "netboxxx",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "Generate configurations from NetBox IPAM prefixes",
+	// Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		host := viper.GetString("host")
